01e754860139150f1569c49ee3040ba0ca162e76
Jay Chetty <jaypaulchetty@gmail.com>
2013-07-10 14:01:07 +0100

    basic with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 StaticBootstrapApp::Application.routes.draw do
+
+
+  root to: 'static_pages#home'
+
+  match 'home', to: 'static_pages#home'
+  match 'about', to: 'static_pages#about'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
