356390b44f527a83028311bb3dfc8e76f3dd1f8b
Jay Chetty <jaypaulchetty@gmail.com>
2013-07-10 17:41:42 +0100

    me page template set

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ StaticBootstrapApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match 'home', to: 'static_pages#home'
-  match 'about', to: 'static_pages#about'
+  match 'contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
